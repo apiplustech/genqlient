bf1eab1faf91de4653490968df9887fbdf1ebf61
surin-api <surin.j@apiplustech.com>
2024-06-27 17:38:39 +0700

    remove marshal for Upload

--- graphql/upload.go
+++ graphql/upload.go
@@ -1,16 +1,10 @@
 package graphql
 
 import (
-	"encoding/json"
 	"io"
 )
 
 type Upload struct {
 	FileName string
 	Body     io.Reader
-}
-
-// MarshalJSON implements json.Marshaler.
-func (Upload) MarshalJSON() ([]byte, error) {
-	return json.Marshal(nil)
 }
